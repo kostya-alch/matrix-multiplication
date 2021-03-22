f03e06eb32824460eb5b7c5b5e4747cfc82dc769
kostya-alch <kostafrompiter@gmail.com>
2021-03-23 01:49:48 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(120, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(177, 98);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(120, 116);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(177, 101);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(442, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(120, 223);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.Size = new System.Drawing.Size(177, 107);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.Location = new System.Drawing.Point(547, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ввод данных с диска";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(651, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 55);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Вывод ответа в файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 431);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Перемножение матриц N*M и M*N";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Функция умножения матриц
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Массив для ответа
+            int[,] matrixC = new int[Convert.ToInt32(dataGridView1.RowCount),Convert.ToInt32(dataGridView2.ColumnCount)];
+            dataGridView3.ColumnCount = dataGridView2.ColumnCount;
+            dataGridView3.RowCount = dataGridView1.RowCount;
+            // проверка на то, что число столбцов матрицы 1 равно строк в матрице 2
+            if (dataGridView1.ColumnCount != dataGridView2.RowCount)
+            {
+               MessageBox.Show("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+            }
+            // цикл умножения элементов матриц
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView2.ColumnCount; j++)
+                {
+                    matrixC[i, j] = 0;
+
+                    for (int k = 0; k < dataGridView1.ColumnCount; k++)
+                    {
+                        matrixC[i, j] += Convert.ToInt32(dataGridView1.Rows[i].Cells[k].Value) * Convert.ToInt32(dataGridView2.Rows[k].Cells[j].Value);
+                    }
+                }
+            }
+            // заполнение матрицы 3
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView2.ColumnCount; j++)
+                {
+                    dataGridView3.Rows[i].Cells[j].Value = matrixC[i, j];
+                }
+            }
+        }
+        //функция вывода содержимого текстовых файлов
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // считывание строк с текстовых файлов
+            string[] line = File.ReadAllLines(@"1_1.txt").ToArray();
+            string[] lines = File.ReadAllLines(@"1_2.txt").ToArray();
+            // считывание первых строк с текстовых файлов
+            string Line = File.ReadLines(@"1_1.txt").First();
+            string Line_S = File.ReadLines(@"1_2.txt").First();
+            // разбиение на символы матриц  с разделителями \n
+            string[] str = Line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] str_S = Line_S.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            // считывание длины строк
+            int rowss = File.ReadAllLines(@"1_1.txt").Length, rows_s = File.ReadAllLines(@"1_2.txt").Length;
+            // присваивание значений переменной
+            int columns = str.Length, columns_s = str_S.Length;
+            // создание строчек и столбцов
+            dataGridView1.ColumnCount = columns;
+            dataGridView1.RowCount = rowss;
+            // создание строчек и столбцов
+            dataGridView2.ColumnCount = columns_s;
+            dataGridView2.RowCount = rows_s;
+            // заполнение матрицы 1 
+            for (int i = 0; i < rowss; i++)
+            {
+                int[] row = line[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = row[j];
+                }
+            }
+            // заполнение матрицы 2
+            for (int i = 0; i < rows_s; i++)
+            {
+                int[] rows = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+                for (int j = 0; j < columns_s; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = rows[j];
+                }
+            }
+        }
+        // функция вывода ответа в файл
+        private void button3_Click(object sender, EventArgs e)
+        { // открытие и запмсь в файл 
+            StreamWriter file = new StreamWriter("1_3.txt");
+            // цикл записи в файл
+            for (int i = 0; i < dataGridView3.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView3.ColumnCount; j++)
+                {
+                    file.Write(Convert.ToInt32(dataGridView3.Rows[i].Cells[j].Value) + " ");
+                }
+                file.Write(Environment.NewLine);
+            } 
+            //закрытые файла
+            file.Close();
+        }
+        // функция печати
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PrintDialog printDialog1 = new PrintDialog();
+            PrintDocument printDocument1 = new PrintDocument();
+            printDocument1.PrintPage += new PrintPageEventHandler(printDocument1_PrintPage);
+            printDialog1.ShowDialog();
+            printDocument1.PrinterSettings = printDialog1.PrinterSettings;
+            printDocument1.Print();
+        }
+        // функция печати
+        void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            StreamReader TF = new StreamReader("1_3.txt", Encoding.Default);
+            string TFF = TF.ReadToEnd();
+            TF.Dispose();
+            e.Graphics.DrawString(TFF, Font, new SolidBrush(Color.Black), new RectangleF(20, 20, 800, 600));
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
